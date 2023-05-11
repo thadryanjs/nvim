b274868206384da3aacdcb37a70e274ef407e6a2
thadryanjs <thadryan@protonmail.com>
2023-05-11 13:03:24 -0400

    checkin

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -17,10 +17,10 @@ return {
       },
       transparent_background = false,
     }
-    vim.cmd.colorscheme 'catppuccin'
-    local colors = require('catppuccin.palettes.mocha')
-    vim.api.nvim_set_hl(0, 'Tabline', {fg = colors.green, bg = colors.mantle})
-    vim.api.nvim_set_hl(0, 'TermCursor', {fg =  '#A6E3A1', bg = '#A6E3A1'})
+    -- vim.cmd.colorscheme 'catppuccin'
+    -- local colors = require('catppuccin.palettes.mocha')
+    -- vim.api.nvim_set_hl(0, 'Tabline', {fg = colors.green, bg = colors.mantle})
+    -- vim.api.nvim_set_hl(0, 'TermCursor', {fg =  '#A6E3A1', bg = '#A6E3A1'})
   end
   },
 
